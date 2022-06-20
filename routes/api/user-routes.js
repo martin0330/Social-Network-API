f87b70b8a234079c4ff271b92c5423b7f94c52e4
martin0330 <martin.cruz0330@yahoo.com>
2022-06-20 14:52:34 -0500

    added user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -9,3 +9,21 @@ const {
     createFriend,
     deleteFriend
 } = require('../../controllers/user-controller')
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route('./:userId')
+    .get(getUserById)
+    .put(updateUserById)
+    .delete(deleteUserById);
+
+router
+    .route('./:userId/friends/:friendId')
+    .post(createFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
