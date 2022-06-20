7df453b51d70f2c397fb5bcb8a3119abca33f816
martin0330 <martin.cruz0330@yahoo.com>
2022-06-20 14:11:53 -0500

    moved reaction to thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const ThoughtSchema = new Schema({
     thoughtText: {
@@ -17,6 +18,28 @@ const ThoughtSchema = new Schema({
         required: true
     },
     reactions: [reactionSchema]
+})
+
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
 })
 
 thoughtSchema.virtual('reactionCount').get(function () {
