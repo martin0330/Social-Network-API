d17645fd19b800af64b3aca506d02d5b6bb96078
martin0330 <martin.cruz0330@yahoo.com>
2022-06-21 19:54:32 -0500

    added thought-controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,51 @@
 const { Thought, User } = require('../models')
+
+const thoughtController = {
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: "No thought found with this id." });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    createReaction({ params, body }, res) {
+        console.log(params);
+        Thought.create(body)
+            .then(({ _id }) => {
+                return reaction.findOneAndUpdate(
+                    { _id: params.thoughtId },
+                    { $push: { comments: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                console.log(dbUserData);
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+        }
+}
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const userRoutes = require('./api/user-routes')
 const thoughtRoutes = require('./api/thought-routes')
 
 router.use('/user', userRoutes);
-
 router.use('/thought', thoughtRoutes);
 
 module.exports = router
